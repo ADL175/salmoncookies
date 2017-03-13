d06a78d2704978ca5ccd46b74de72473eb28542d
ADL175 <armydavidlim@gmail.com>
2017-03-13 13:10:23 -0700

    commit

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '8pm', '9pm']
+
+var cookieStore = {
+  name: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  someFunk (){
+    some return
+  }
+};
+
+var cookieStore = {
+  name: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  someFunk (){
+    some return
+  }
+};
+
+var cookieStore = {
+  name: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  someFunk (){
+    some return
+  }
+};
+
+var cookieStore = {
+  name: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+  someFunk (){
+    some return
+  }
+};
+
+var cookieStore = {
+  name: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+  someFunk (){
+    some return
+  }
+};
