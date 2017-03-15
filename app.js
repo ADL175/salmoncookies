a7431af05223cff32e2d74415eb38b9e03a7d6ca
ADL175 <armydavidlim@gmail.com>
2017-03-15 13:36:31 -0700

    fixing

--- app.js
+++ app.js
@@ -1,236 +1,77 @@
 'use strict';
 
+var myStores = [];
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '8pm', 'total'];
 
 var body = document.getElementsByTagName('body')[0];
+//var body = document.body;
 
-var firstAndPike = {
-  name: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
-  avgCookies: 6.3,
-  salesArr: [],
-  randCust: function(){
-    //generate random # of cust
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    console.log(this.randCust);
-  },
-  cookiesSold: function(){
+function Store(storeName, minCust, maxCust, avgCookies){
+  this.name = storeName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookies = avgCookies;
+  this.salesArr = [];
+  this.randCust = function(){
+        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+        console.log(this.randCust);
+  };
+  this.cookiesSold = function(){
     var total = 0;
     for(var i = 0; i < storeHours.length -1; i++) {
-    //-1 of storeHours array [total]
-    var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
-    //generates cookies sales total
-    this.salesArr.push(cookiesPerHour);
-    total += cookiesPerHour;
+      var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
+      this.salesArr.push(cookiesPerHour);
+      total += cookiesPerHour;
     }
     this.salesArr.push(total);
     console.log(this.salesArr);
     //adds total number of sales for day to the sales array
-  },
-  createListItems: function(){
-    //create li elements and push them to specified to element
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
-    body.appendChild(newHeading);
-    var firstUl = document.createElement('ul');
-    for(var i = 0; i < storeHours.length; i++) {
-      //for every hour I ewant to put put the cookies sold for that hour;
-      var newEl = document.createElement('li'); //creates list item
-      newEl.innerText = storeHours[i] + ': ' + this.salesArr[i] + ' cookies';
-      firstUl.appendChild(newEl);
-      //append the new list item to the ul tag that we created
+  };
+  this.generateTableRow= function(){
+    this.cookiesSold();
+    var table = document.getElementsByTagName('table')[0];
+     var tbody = document.createElement('tbody');
+     table.appendChild(tbody);
+     var tr = document.createElement('tr');
+     tbody.appendChild(tr);
+     var tData = document.createElement('td');
+     tr.appendChild(tData);
+     tData.innerText = this.name;
+      for(var i = 0; i < storeHours.length; i++){
+          var td = document.createElement('td');
+          td.innerText = this.salesArr[i];
+          tr.appendChild(td);
+        }
+        table.appendChild(tbody);
     }
-    body.appendChild(firstUl);
-    //append the ul tag to the body
+    myStores.push(this);//this was put in the old version w/o for loop
+}
+
+function renderHeader (){
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+  var blank = document.createElement('th');
+  thead.appendChild(tr);
+  thead.appendChild(blank);
+  table.appendChild(thead);
+  body.appendChild(table);
+  blank.innerText = 'Store Name';
+  for(var i = 0; i < storeHours.length; i++){
+    var th = document.createElement('th');
+    th.innerText = storeHours[i];
+    thead.appendChild(th);
   }
-  // var li = document.createElement('li');
-  // li.innerText = 'Total :' + firstAndPike.total;
 };
 
-firstAndPike.cookiesSold();//run the cookies sold method
-firstAndPike.createListItems();
+renderHeader();
 
-var seatac = {
-  name: 'SeaTac',
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  salesArr: [],
-  randCust: function(){
-    //generate random # of cust
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    console.log(this.randCust);
-  },
-  cookiesSold: function(){
-    var total = 0;
-    for(var i = 0; i < storeHours.length -1; i++) {
-    //-1 of storeHours array [total]
-    var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
-    //generates cookies sales total
-    this.salesArr.push(cookiesPerHour);
-    total += cookiesPerHour;
-    }
-    this.salesArr.push(total);
-    console.log(this.salesArr);
-    //adds total number of sales for day to the sales array
-  },
-  createListItems: function(){
-    //create li elements and push them to specified to element
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
-    body.appendChild(newHeading);
-    var firstUl = document.createElement('ul');
-    for(var i = 0; i < storeHours.length; i++) {
-      //for every hour I ewant to put put the cookies sold for that hour;
-      var newEl = document.createElement('li'); //creates list item
-      newEl.innerText = storeHours[i] + ': ' + this.salesArr[i] + ' cookies';
-      firstUl.appendChild(newEl);
-      //append the new list item to the ul tag that we created
-    }
-    body.appendChild(firstUl);
-    //append the ul tag to the body
-  }
-
-};
-
-seatac.cookiesSold();//run the cookies sold method
-seatac.createListItems();
-
-var seattleCenter = {
-  name: 'Seattle Center',
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  salesArr: [],
-  randCust: function(){
-    //generate random # of cust
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    console.log(this.randCust);
-  },
-  cookiesSold: function(){
-    var total = 0;
-    for(var i = 0; i < storeHours.length -1; i++) {
-    //-1 of storeHours array [total]
-    var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
-    //generates cookies sales total
-    this.salesArr.push(cookiesPerHour);
-    total += cookiesPerHour;
-    }
-    this.salesArr.push(total);
-    console.log(this.salesArr);
-    //adds total number of sales for day to the sales array
-  },
-  createListItems: function(){
-    //create li elements and push them to specified to element
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
-    body.appendChild(newHeading);
-    var firstUl = document.createElement('ul');
-    for(var i = 0; i < storeHours.length; i++) {
-      //for every hour I ewant to put put the cookies sold for that hour;
-      var newEl = document.createElement('li'); //creates list item
-      newEl.innerText = storeHours[i] + ': ' + this.salesArr[i] + ' cookies';
-      firstUl.appendChild(newEl);
-      //append the new list item to the ul tag that we created
-    }
-    body.appendChild(firstUl);
-    //append the ul tag to the body
-  }
-
-};
-
-seattleCenter.cookiesSold();//run the cookies sold method
-seattleCenter.createListItems();
-
-var capitolHill = {
-  name: 'Capitol Hill',
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  salesArr: [],
-  randCust: function(){
-    //generate random # of cust
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    console.log(this.randCust);
-  },
-  cookiesSold: function(){
-    var total = 0;
-    for(var i = 0; i < storeHours.length -1; i++) {
-    //-1 of storeHours array [total]
-    var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
-    //generates cookies sales total
-    this.salesArr.push(cookiesPerHour);
-    total += cookiesPerHour;
-    }
-    this.salesArr.push(total);
-    console.log(this.salesArr);
-    //adds total number of sales for day to the sales array
-  },
-  createListItems: function(){
-    //create li elements and push them to specified to element
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
-    body.appendChild(newHeading);
-    var firstUl = document.createElement('ul');
-    for(var i = 0; i < storeHours.length; i++) {
-      //for every hour I ewant to put put the cookies sold for that hour;
-      var newEl = document.createElement('li'); //creates list item
-      newEl.innerText = storeHours[i] + ': ' + this.salesArr[i] + ' cookies';
-      firstUl.appendChild(newEl);
-      //append the new list item to the ul tag that we created
-    }
-    body.appendChild(firstUl);
-    //append the ul tag to the body
-  }
-
-};
-
-capitolHill.cookiesSold();//run the cookies sold method
-capitolHill.createListItems();
-
-var alki = {
-  name: 'Alki',
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  salesArr: [],
-  randCust: function(){
-    //generate random # of cust
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    console.log(this.randCust);
-  },
-  cookiesSold: function(){
-    var total = 0;
-    for(var i = 0; i < storeHours.length -1; i++) {
-    //-1 of storeHours array [total]
-    var cookiesPerHour = Math.floor(this.avgCookies * this.randCust());
-    //generates cookies sales total
-    this.salesArr.push(cookiesPerHour);
-    total += cookiesPerHour;
-    }
-    this.salesArr.push(total);
-    console.log(this.salesArr);
-    //adds total number of sales for day to the sales array
-  },
-  createListItems: function(){
-    //create li elements and push them to specified to element
-    var newHeading = document.createElement('h2');
-    newHeading.innerText = this.name;
-    body.appendChild(newHeading);
-    var firstUl = document.createElement('ul');
-    for(var i = 0; i < storeHours.length; i++) {
-      //for every hour I ewant to put put the cookies sold for that hour;
-      var newEl = document.createElement('li'); //creates list item
-      newEl.innerText = storeHours[i] + ': ' + this.salesArr[i] + ' cookies';
-      firstUl.appendChild(newEl);
-      //append the new list item to the ul tag that we created
-    }
-    body.appendChild(firstUl);
-    //append the ul tag to the body
-  }
-
-};
-
-alki.cookiesSold();//run the cookies sold method
-alki.createListItems();
+var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 3.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+//
+for (var i = 0; i < myStores.length; i++) {
+  myStores[i].generateTableRow();
+}
