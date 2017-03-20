5db993d35ac3fa74752a375ad1e57e090a1ff400
ADL175 <armydavidlim@gmail.com>
2017-03-19 19:26:06 -0700

    having trouble w/ fixing hourly totals after user submits data, reorganized to make code more readable

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 
+////////////////////////////////////////////////////////////
+// Configuration
+////////////////////////////////////////////////////////////
+
 var storeHours = [
   '6am',
   '7am',
@@ -18,25 +22,29 @@ var storeHours = [
   '8pm',
   'total'];
 
+//Current and new store will be pushed into this array
 var myStores = [];
+
+//current stores using object constructor
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
 var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 3.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+//DOM manimpulation variables
 var table = document.createElement('table');
 var tbody = document.createElement('tbody');
-/*Object Constructor to build the stores:
-function store =
-  contains:
-    numCustomersPerHour = randomly genereated customers per hour w/ in limits of minCustomersPerHour and maxCustomersPerHour
-    cookiesPerHour
-*/
+
+////////////////////////////////////////////////////////////
+// Configuration
+// Object Constructor, to create defined stores and take data from user input
+// to create a new store and generate sales data.
+////////////////////////////////////////////////////////////
 function Store(storeName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer){
-  this.name = storeName;
-  this.minCustomersPerHour = minCustomersPerHour;
-  this.maxCustomersPerHour = maxCustomersPerHour;
+  this.name                  = storeName;
+  this.minCustomersPerHour   = minCustomersPerHour;
+  this.maxCustomersPerHour   = maxCustomersPerHour;
   this.avgCookiesPerCustomer = avgCookiesPerCustomer;
 //Cookes Sold for each hour of the store hours, total sum is last item
   this.populateCookiesSoldPerHour = [];
@@ -56,81 +64,108 @@ function Store(storeName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPe
   };
   this.appendTableRow = function(){
     this.cookiesPerHour();
-    var tr = document.createElement('tr');
-    tbody.appendChild(tr);
-    var tData = document.createElement('td');
-    tr.appendChild(tData);
-    tData.innerText = this.name;
+    var trStoreName = document.createElement('tr');
+    var tdSalesPerHour = document.createElement('td');
+    tbody.appendChild(trStoreName);
+    trStoreName.appendChild(tdSalesPerHour);
+    tdSalesPerHour.innerText = this.name;
+
     for(var i = 0; i < storeHours.length; i++){
       var td = document.createElement('td');
       td.innerText = this.populateCookiesSoldPerHour[i];
-      tr.appendChild(td);
+      trStoreName.appendChild(td);
     }
     table.appendChild(tbody);
   };
   myStores.push(this);
 }
 
+////////////////////////////////////////////////////////////
+// Creates Table Headers w/ store hours
+////////////////////////////////////////////////////////////
+var form = document.getElementById('the-form');
+
 function renderSalesTableHeader (){
-  var tableBody = document.getElementsByTagName('body')[0];
-  var headerStoreHours = document.createElement('thead');
-  var tr = document.createElement('tr');
-  var storePlaceholder = document.createElement('th');
+  var tableBody         = document.getElementsByTagName('body')[0];
+  var headerStoreHours  = document.createElement('thead');
+  var tr                = document.createElement('tr');
+  var storePlaceholder  = document.createElement('th');
+  storePlaceholder.innerText = 'Store Name';
+
   headerStoreHours.appendChild(tr);
   headerStoreHours.appendChild(storePlaceholder);
   table.appendChild(headerStoreHours);
   tableBody.appendChild(table);
-  storePlaceholder.innerText = 'Store Name';
+
   for(var i = 0; i < storeHours.length; i++){
-    var th = document.createElement('th');
-    th.innerText = storeHours[i];
+    var th        = document.createElement('th');
+    th.innerText  = storeHours[i];
     headerStoreHours.appendChild(th);
   }
 };
-//for loop calls methods to add to table
-for (var i = 0; i < myStores.length; i++) {
-  myStores[i].appendTableRow();
-}
-//create JS events for user entry
-var form = document.getElementById('the-form');
-
-function userSubmitButtonEvent(event){
-  event.preventDefault();
-  var theFormItself = event.target;
-  var storeName = theFormItself.elements['storeName'].value;
-  var minCustomersPerHour = Math.floor(theFormItself.elements['minCustomersPerHour'].value);
-  var maxCustomersPerHour = Math.floor(theFormItself.elements['maxCustomersPerHour'].value);
-  var avgCookiesPerCustomer = theFormItself.elements['avgCookiesPerCustomer'].value;
-  if(maxCustomersPerHour < minCustomersPerHour){
-    confirm('You put the maximum sales is less than minimum sales');
-  }else{
-    var userStore = new Store(storeName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerCustomer);
-    var table = document.getElementsByTagName('table')[0];
-    var totalsRow = document.getElementsByTagName('table')[0];
-    // tfoot.removeChild(totalsRow);//remove existing total row
-    userStore.appendTableRow();
-  }
-  form.reset();
-};
-form.addEventListener('submit', userSubmitButtonEvent);
-// Create Footer Total Function
-
+////////////////////////////////////////////////////////////
+// Creates Table Footers w/ store hourly totals
+////////////////////////////////////////////////////////////
 function renderSalesTotalFooter(){
-  var tFootDataTotals = document.createElement('tr');
+  var tFootTotal      = document.createElement('tfoot');
+  var tRowDataTotals  = document.createElement('tr');
   var tableDataTotals = document.createElement('td');
+
+  tRowDataTotals.appendChild(tableDataTotals);
+  tFootTotal.appendChild(tRowDataTotals);
+  table.appendChild(tFootTotal);
+
   tableDataTotals.innerText = 'Hourly Totals';
-  tFootDataTotals.appendChild(tableDataTotals);
+//For loop to generate hourly totals for all stores
   for(var i = 0; i < storeHours.length; i++){
     var hourlyTotal = 0;
     for(var j = 0; j < myStores.length; j++){
       hourlyTotal += myStores[j].populateCookiesSoldPerHour[i];
     }
-    tableDataTotals = document.createElement('td');
+
+    tableDataTotals           = document.createElement('td');
     tableDataTotals.innerText = hourlyTotal;
-    tFootDataTotals.appendChild(tableDataTotals);
+    tRowDataTotals.appendChild(tableDataTotals);
   }
-  tbody.appendChild(tFootDataTotals);
+  tbody.appendChild(tRowDataTotals);
 }
-//calling header footer function
+
+////////////////////////////////////////////////////////////
+// User Initializes by Submit Button Event
+////////////////////////////////////////////////////////////
+
+function userSubmitButtonEvent(event){
+  event.preventDefault();
+  var storeName       = event.target.elements['storeName'].value;
+  var minCustomersPerHour = Math.floor(event.target.elements['minCustomersPerHour'].value);
+  var maxCustomersPerHour = Math.floor(event.target.elements['maxCustomersPerHour'].value);
+  var avgCookiesPerCustomer = event.target.elements['avgCookiesPerCustomer'].value;
+//To prevent error from user input, if user enters bad data,
+//program will alert user to enter good data
+  if(maxCustomersPerHour < minCustomersPerHour){
+    confirm('You put the maximum sales is less than minimum sales');
+  }else{
+    var userStore = new Store(
+      storeName,
+      minCustomersPerHour,
+      maxCustomersPerHour,
+      avgCookiesPerCustomer);
+    var table = document.getElementsByTagName('table')[0];
+    var totalsRow = document.getElementsByTagName('table')[0];
+    userStore.appendTableRow();
+  }
+  form.reset();
+};
+form.addEventListener('submit', userSubmitButtonEvent);
+
+//////////////////////////////////////
+/////////////////// Execute Program
+//////////////////////////////////////
+
+//for loop calls methods to add to table
+for (var i = 0; i < myStores.length; i++) {
+  myStores[i].appendTableRow();
+}
+//call table rendering DOM manipulation
 renderSalesTableHeader();
 renderSalesTotalFooter();
